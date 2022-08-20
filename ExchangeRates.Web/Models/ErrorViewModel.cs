e30464fb4c94e5ccbe235d70737506c20e75ac27
KTomczak <ktomczak1996@gmail.com>
2022-08-20 15:16:00 +0200

    initial

---
+++ ExchangeRates.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ExchangeRates.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
